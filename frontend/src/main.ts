7ad7dd56b0e999a7c62bb8469fe96cda79154fd9
Stan Drozd <drozdziak1@gmail.com>
2021-09-20 19:05:23 +0200

    frontend: Update stream links

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,15 +6,15 @@ const app = new App({
 	props: {
 	  streams: [
 	    {
-	      src: "https://pisk.miau.how/stream_0",
+	      src: "https://pisk.miau.how/raw/stream_0",
 	      description: "This Sputnik is the central Rat HQ. Located in the right-upper-front corner of the cage it can fit the whole crew!";
 	    },
 	    {
-	      src: "https://pisk.miau.how/stream_1",
+	      src: "https://pisk.miau.how/raw/stream_1",
 	      description: "The ferret tunnel - Pliszka and Gąska's favorite",
 	    },
 	    {
-	      src: "https://pisk.miau.how/stream_2",
+	      src: "https://pisk.miau.how/raw/stream_2",
 	      description: "The bottom. Sarna (albino thicc queen) loves the wooden house on the left.",
 	    }
 	  ],
